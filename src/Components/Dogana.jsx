5da3a9ce8ee39960a84e62770dd07b9f919b3bca
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-11 22:59:38 +0200

    final version

--- src/Components/Dogana.jsx
+++ src/Components/Dogana.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useMemo, useState } from "react";
 import "./Dogana.css";
 
 
@@ -24,9 +24,9 @@ const Dogana = () =>
         const vleraNum = Number(vlera) || 0;
         const bracket = Akciza[kubikIndex];
         const akciza = vetureERe ? bracket.akciza * 1.2 : bracket.akciza;
-        const tatimiNeImport = vleraNum * tatimi;
+        const tatimiNeImport = vleraNum * Tatimi;
          const tvsh = (vleraNum + akciza + tatimiNeImport) * Tvsh;
-        const dogana = akciza + Tatimi + tvsh;
+      const dogana = akciza + tatimiNeImport + tvsh;
          const totali = vleraNum + dogana;
 
          return{akciza: Math.round(akciza),
@@ -42,58 +42,92 @@ const Dogana = () =>
       <div className="kd-card"><h1>Kalkulo doganen per veturen tuaj</h1>
       <div className="kd-row">
         <label className="kd-label">Viti prodhimit</label>
-        <select className="kd-input"><option></option></select>
+        <select className="kd-input" value={viti}
+        onChange={(e) =>setViti(Number(e.target.value))}>
+          {Years.map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
       </div>
+
       <div className="kd-row kd-row-checkbox">
         <span className="kd-label"></span>
-        <label>Veture e re<input></input></label>
+        <label className="kd-checkbox-label">Veture e re<input type="checkbox"
+        checked={vetureERe}
+        onChange={(e)=>setVetureERe(e.target.checked)}></input>Vetur e re</label>
       </div>
+
       <div className="kd-row"><label className="kd-label">Vlera e vetures</label>
       <div className="kd-input-euro">
-        <input type="number" className="kd-input"></input>
+        <input type="number" className="kd-input"
+        type="number"
+              className="kd-input"
+              value={vlera}
+              min={0}
+              onChange={(e) => setVlera(e.target.value)}></input>
         <span className="kd-euro">€</span>
       </div>
       </div>
+
       <div className="kd-row">
         <label className="kd-label">Kubik(cmm)</label>
-        <select className="kd-input">
-          <option></option>
+        <select className="kd-input"
+        value={kubikIndex}
+            onChange={(e) => setKubikIndex(Number(e.target.value))}>
+            {Akciza.map((b, i) => (
+              <option key={b.label} value={i}>
+                {b.label}
+              </option>
+            ))}
         </select>
       </div>
       <div className="kd-divider">----------</div>
       <div className="kd-row">
         <label className="kd-label">Akciza:</label>
-        <div className="kd-input-euro"></div>
+        <div className="kd-input-euro">
+          <div className="kd-result">{formatEuro(rezultatet.akciza)}</div>
+        </div>
         <span className="kd-euro">€</span>
       </div>
-       </div>
+    
+
+
        <div className="kd-row">
         <label className="ld-label">Tatimi ne import</label>
         <div className="kd-input-euro">
-          <div className="kd-result"></div>
+          <div className="kd-result">{formatEuro(rezultatet.tatimiNeImport)}</div>
           <span className="kd-euro">€</span>
         </div>
        </div>
+
        <div className="kd-row">
         <label className="kd-label">TVSH-ja</label>
         <div className="kd-input-euro">
-          <div className="kd-result"></div>
+          <div className="kd-result">{formatEuro(rezultatet.tvsh)}</div>
           <span>€</span>
         </div>
        </div>
          <div className="kd-row">
-        <label className="kd-label">Dogana</label>
+        <label className="kd-label">  Dogana (Akciza + Tatimi
+            <br />
+            në import + TVSH) :</label>
         <div className="kd-input-euro">
-          <div className="kd-result kd-result-orange"></div>
-          <span className="kd-euro kd-euro-orange">€</span>
+          <div className="kd-result kd-result-green">{formatEuro(rezultatet.dogana)}</div>
+          <span className="kd-euro kd-euro-green">€</span>
         </div>
        </div>
          <div className="kd-row">
-        <label className="kd-label kd-label-orange">Totali</label>
+        <label className="kd-label kd-label-green">( Vetura + Dogana ) :</label>
         <div className="kd-input-euro">
-          <div className="kd-result kd-result-orange"></div>
-          <span className="kd-euro kd-euro-orange">€</span>
+          <div className="kd-result kd-result-green">
+            {formatEuro(rezultatet.totali)}
+          </div>
+          <span className="kd-euro kd-euro-green">€</span>
         </div>
        </div>
-      </div>)
+      </div>
+         </div>)
   }
+  export default Dogana;
