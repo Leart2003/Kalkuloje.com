94548ec06a41637a22562f207251850bcdca18cb
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-12 22:33:17 +0200

    using react router instead of calling component directly

---
+++ src/Pages/KalkuloDoganen.js
@@ -0,0 +1,11 @@
+import Dogan from "./Components/Dogana.jsx";
+
+
+const KalkuloDoganen = () => 
+  {
+    return(
+      <div>
+        <Dogan/>
+      </div>
+    )
+  }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Link, Route } from 'react-router'
 
 import Dogana from './Components/Dogana.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <Dogana/ >
+  <Route path="/kalkulo-doganen" element={<Dogan />} />
   </StrictMode>,
 )
